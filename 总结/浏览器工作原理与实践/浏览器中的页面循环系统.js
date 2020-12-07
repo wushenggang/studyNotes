4ee956eaf16966a58264f354d613f7c74c6c4881
wsg <315810842@qq.com>
2020-12-07 20:08:23 +0800

    update

--- 总结/浏览器工作原理与实践/浏览器中的页面循环系统.js
+++ 总结/浏览器工作原理与实践/浏览器中的页面循环系统.js
@@ -47,3 +47,16 @@ JavaScript 引擎会检查全局执行上下文中的微任务队列，然后按
 2，微任务的执行时长会影响到当前宏任务的时长。
 如果采用 setTimeout 创建宏任务来触发回调的话, 在两个任务之间，可能会被渲染进程插入其他的事件，从而影响到响应的实时性。
 MutationObserver 采用了“异步 + 微任务”的策略。
+
+19:
+产生地狱回调的原因：1，多层嵌套的问题 2，每种任务的处理结果存在两种可能性（成功或失败），那么需要在每种任务执行结束后分别处理这两种可能性。
+Promise 通过回调函数延迟绑定、回调函数返回值穿透和错误“冒泡”技术解决了上面的两个问题。
+1，Promise中为什么要引入微任务？
+由于promise采用.then延时绑定回调机制，而new Promise时又需要直接执行promise中的方法，即发生了先执行方法后添加回调的过程，此时需等待then方法绑定两个回调后才能继续执行方法回调，
+便可将回调添加到当前js调用栈中执行结束后的任务队列中，由于宏任务较多容易堵塞，则采用了微任务
+2、Promise 中是如何实现回调函数返回值穿透的？
+首先Promise的执行结果保存在promise的data变量中，然后是.then方法返回值为使用resolved或rejected回调方法新建的一个promise对象，即例如成功则返回new Promise（resolved），
+将前一个promise的data值赋给新建的promise
+3、Promise 出错后，是怎么通过“冒泡”传递给最后那个捕获
+promise内部有resolved_和rejected_变量保存成功和失败的回调，进入.then（resolved，rejected）时会判断rejected参数是否为函数，若是函数，错误时使用rejected处理错误；
+若不是，则错误时直接throw错误，一直传递到最后的捕获，若最后没有被捕获，则会报错。可通过监听unhandledrejection事件捕获未处理的promise错误。
