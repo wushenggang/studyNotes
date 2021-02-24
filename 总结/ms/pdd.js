b3ee27b9442b720bba3c23b0972407f852d9a290
wsg <315810842@qq.com>
2021-02-24 20:26:17 +0800

    'update'

--- 总结/ms/pdd.js
+++ 总结/ms/pdd.js
@@ -20,13 +20,13 @@ history:
 pushState 配合 popstate 监听。但因为没有 # 号，所以当用户刷新页面之类的操作时，浏览器还是会给服务器发送请求。为了避免出现这种情况，
 所以这个实现需要服务器的支持，需要把所有路由都重定向到根页面。
 
-vue-router实现原理: https://zhuanlan.zhihu.com/p/37730038
+vue - router实现原理: 我们需要对current.route做数据劫持。一旦current.route更新，我们可以及时的更新当前页面(https://zhuanlan.zhihu.com/p/37730038)
 
-vuex的使用
+  vuex的使用
 父传子，子传父的用法
 eventHUb的原理（手写发布订阅模式）
 
-用过哪些ES6
+  用过哪些ES6
 Promise.all()
 Promise.race()
 
@@ -37,14 +37,28 @@ vue的生命周期
 父组件中有子组件。生命周期是咋样的
 
 一、加载渲染过程
-父beforeCreate->父created->父beforeMount->子beforeCreate->子created->子beforeMount->子mounted->父mounted
+父beforeCreate -> 父created -> 父beforeMount -> 子beforeCreate -> 子created -> 子beforeMount -> 子mounted -> 父mounted
 二、子组件更新过程
-父beforeUpdate->子beforeUpdate->子updated->父updated
+父beforeUpdate -> 子beforeUpdate -> 子updated -> 父updated
 三、父组件更新过程
-父beforeUpdate->父updated
+父beforeUpdate -> 父updated
 四、销毁过程
-父beforeDestroy->子beforeDestroy->子destroyed->父destroyed
+父beforeDestroy -> 子beforeDestroy -> 子destroyed -> 父destroyed
 
 是否用过keep - alive
-
-
+keep - alive是Vue.js的一个内置组件。它能够将不活动的组件实例保存在内存中，而不是直接将其销毁
+用法是将需要缓存的组件用 < keep - alive > 标签包裹起来。include与exclude两个属性来控制缓存哪些组件，不缓存哪些组件或者配置router.meta 属性，
+  在 < router - view v -if= "$route.meta.keepAlive" > 中进行判断是否缓存
+activated()函数就是一个页面激活后的钩子函数，一进入页面就触发(如果已经有了缓存就不会再一次created和mounted)
+deactivated() 页面关闭时触发
+场景： 一个填写表单的页面。其中一个选项后需要打开新的页面来选定。当选择好后返回表单页面时需要能够保留之前填写的数据。
+beforeRouteLeave(to, from, next) {
+  // 设置下一个路由的 meta
+  to.meta.keepAlive = true;  // 让 A 缓存，即不刷新
+  next();
+}
+通过beforeRouteLeave可以做很多事
+实现原理：created钩子会创建一个cache对象，用来作为缓存容器，保存vnode节点。destroyed钩子则在组件被销毁的时候清除cache缓存中的所有组件实例。
+接下来该组件的name（存在组件名则直接使用组件名，否则会使用tag）。接下来会将这个name通过include与exclude属性进行匹配，匹配不成功（说明不需要进行缓存）则不进行任何操作直接返回vnode。
+假如匹配成功，根据key在this.cache中查找，如果存在则说明之前已经缓存过了，直接将缓存的vnode的componentInstance（组件实例）覆盖到目前的vnode上面。否则将vnode存储在cache中。
+最后返回vnode（有缓存时该vnode的componentInstance已经被替换成缓存中的了）。用watch来监听pruneCache与pruneCache这两个属性的改变，在改变的时候修改cache缓存中的缓存数据。
--- 总结/ms/xl.js
+++ 总结/ms/xl.js
@@ -1,4 +1,66 @@
 如何解决1px问题
+为了适配各种屏幕，我们写代码时一般使用设备独立像素来对页面进行布局。而在设备像素比大于1的屏幕上，我们写的1px实际上是被多个物理像素渲染，这就会出现1px在有些屏幕上看起来很粗的现象。
+1，基于media查询(媒体查询)判断不同的设备像素比给定不同的border - image或者background - image
+  .border_1px{
+  border - bottom: 1px solid #000;
+}
+@media only screen and(-webkit - min - device - pixel - ratio: 2){
+    .border_1px{
+    border - bottom: none;
+    border - width: 0 0 1px 0;
+    border - image: url(../img/1pxline.png) 0 0 2 0 stretch;
+  }
+}
+2，伪类 + transform  通过transform: scale来改变（scale不是改变元素宽高，而是改变x和y轴的刻度）
+
+.border_1px: before{
+  content: '';
+  position: absolute;
+  top: 0;
+  height: 1px;
+  width: 100 %;
+  background - color: #000;
+  transform - origin: 50 % 0 %;
+}
+@media only screen and(-webkit - min - device - pixel - ratio: 2){
+            .border_1px: before{
+    transform: scaleY(0.5);
+  }
+}
+@media only screen and(-webkit - min - device - pixel - ratio: 3){
+            .border_1px: before{
+    transform: scaleY(0.33);
+  }
+}
+3，可以通过svg
+设备像素比(dpr) = 实际物理像素 / 设备独立像素
+
+
+移动端适配方案：
+1，flexible方案（rem）
+rem 是相对于html节点的font - size来做计算的。将html节点的font - size设置为页面clientWidth(布局视口)的1 / 10，
+即1rem就等于页面布局视口的1 / 10，这就意味着我们后面使用的rem都是按照页面比例来计算的。
+然后只需要将UI出的图转换为rem即可。以iPhone6为例：布局视口为375px，则1rem = 37.5px，这时UI给定一个元素的宽为75px（设备独立像素），
+我们只需要将它设置为75 / 37.5 = 2rem。
+当页面大小变化时，也要重新去调整html的fontSize大小。（监听resize，pageshow方法）
+2，vw, vh方法
+vmin: vw和vh中的较小值       vmax: vw和vh中的较大值
+iphoneX的适配：我们必须把页面限制在安全范围内，但是不影响整体效果。viewport - fit是专门为了适配iPhoneX而诞生的一个属性，它用于限制网页如何在安全区域内进行展示。
+contain: 可视窗口完全包含网页内容，cover：网页内容完全覆盖可视窗口。默认情况下为contain。
+我们需要将顶部和底部合理的摆放在安全区域内，iOS11新增了两个CSS函数env、constant，用于设定安全区域与边界的距离。我们必须指定viweport - fit后才能使用这两个函数。constant在iOS < 11.2的版本中生效，env在iOS >= 11.2的版本中生效，这意味着我们往往要同时设置他们，将页面限制在安全区域内：
+body {
+  padding - bottom: constant(safe - area - inset - bottom);
+  padding - bottom: env(safe - area - inset - bottom);
+}
+
+横屏适配：
+js检测横屏：
+window.orientation：获取屏幕旋转方向，(180或者0为竖屏，90或者 - 90为横屏)
+css可以通过媒体查询来检测横屏  @media screen and(orientation: portrait || landscape)
+
+
+
+
 如何提高页面的性能
 工作中遇到的最难的问题
 diff算法
