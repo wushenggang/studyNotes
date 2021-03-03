9e44a94343f4be9c355f036f200aa21d25f0651c
wsg <315810842@qq.com>
2021-03-03 17:22:31 +0800

    'update'

--- 总结/webpack原理与实践/3如何通过loader实现特殊资源加载.js
+++ 总结/webpack原理与实践/3如何通过loader实现特殊资源加载.js
@@ -1,6 +1,6 @@
 开发一个loader:
 创建一个loader文件，此文件需要导出一个函数，这个函数就是我们这个 Loader 对资源的处理过程，它的输入就是加载到的资源文件内容，输出就是我们加工后的结果。输出的内容必须是一段JS代码。
-不然webpack打包后无法解析。(也可以返回别的形式类似于HTML，再交给下一个html - Loader处理，这就涉及到多个loader相互配合工作的情况了。)
+不然webpack打包后无法解析(会把代码放到打包后的文件中，不然语法通不过) 。(也可以返回别的形式类似于HTML，再交给下一个html - Loader处理，这就涉及到多个loader相互配合工作的情况了。)
 完成以后，我们回到 Webpack 配置文件中添加一个加载器规则（use 中不仅可以使用模块名称，还可以使用模块文件路径）。
 
 
@@ -23,3 +23,15 @@ module.exports = source => {
   return code
 
 }
+
+
+// ./src/main.js
+
+import about from './about.md'
+
+
+
+console.log(about)
+
+// 希望 about => '<h1>About</h1><p>this is a markdown file.</p>'
+
