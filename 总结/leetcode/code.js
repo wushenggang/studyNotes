71247c9924ddd6e702b6b12b327ccbb16f14a4fb
wushenggang <315810842@qq.com>
2021-03-07 22:18:54 +0800

    update

--- 总结/leetcode/code.js
+++ 总结/leetcode/code.js
@@ -352,6 +352,45 @@ let maxProfit = (prices)=> {
   return profit
 }
 
+102 二叉树的层序遍历(给你一个二叉树，请你返回其按 层序遍历 得到的节点值)给你一个二叉树，请你返回其按 层序遍历 得到的节点值
 
+dfs   O(n)
+
+var levelOrder = function(root) {
+    if(!root) return []
+    let res = []
+    dfs(root, 0, res)
+    return res
+};
+
+function dfs(root, step, res){
+  if(root){
+    if(!res[step]) res[step] = []
+    res[step].push(root.val)
+    dfs(root.left, step + 1, res)
+    dfs(root.right, step + 1, res)
+  }
+}
+
+bfs  O(n)
+
+var levelOrder = function(root) {
+    if(!root) return []
+    let queue = [root]
+    let res = []
+    while(queue.length > 0){
+      let len = queue.length
+      let arr = []
+      while(len){
+        let node = queue.shift()
+        arr.push(node.val)
+        if(node.left) queue.push(node.left)
+        if(node.right) queue.push(node.right)
+        len--
+      }
+      res.push(arr)
+    }
+    return res
+};
 
 
