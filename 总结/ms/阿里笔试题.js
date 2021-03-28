000645af32e5610b5abbbaf12ea40068c639d5d6
wushenggang <315810842@qq.com>
2021-03-28 21:38:49 +0800

    update

--- 总结/ms/阿里笔试题.js
+++ 总结/ms/阿里笔试题.js
@@ -64,3 +64,42 @@ setTimeout(() => {
 		console.log('done')
 	}
 }, 60000)
+
+
+
+
+1. 工厂模式
+2. 链式调用
+3. 防抖
+4. 优先级队列
+
+return new class {
+
+}
+
+setTimeout(() => console.log(this.taskQueue), 0)
+
+this.taskQueue.push(`nice to meet you ${this.name2}`)
+
+this.taskQueue.push(`nice to meet you ${this.name2}`)
+
+this.taskQueue.unshift(`nice to meet you ${this.name2}`)
+
+实现一个GreetRobot，使之具有以下能力
+1. 执行GreetRobot('bb8')
+输出: Hi this is bb8!
+2. 执行GreetRobot('bb8').greet('Bob')
+输出: Hi this is bb8!
+Nice to meet you Bob.
+3. 执行GreetRobot('bb8').greet('Bob').greet('Jerry')
+输出: Hi this is bb8!
+Nice to meet you Bob.
+Nice to meet you Jerry.
+4. 执行GreetRobot('bb8').greet('Bob').greetVip('Mike')
+输出: Glad to see you Mike~
+Hi this is bb8!
+Nice to meet you Bob.
+5. 执行GreetRobot('bb8').greetVip('Marry').greetVip('Mike')
+输出: Glad to see you Mike~
+Glad to see you Marry~
+Hi this is bb8!
